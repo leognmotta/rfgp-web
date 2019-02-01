ed79db7694965c676a4b2859b0ee0815cbb51a11
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-01 14:24:50 -0500

    MongoDB connect & .env

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${
+      process.env.DB_PASSWORD
+    }@cluster0-sud5s.mongodb.net/${process.env.DB_CLUSTER}`,
+    { useNewUrlParser: true }
+  )
+  .then(result => {
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
